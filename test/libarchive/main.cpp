a7859be638cc75a0ad6c04091d79bc1ab21a233f
kaiser <1244713586@qq.com>
2021-04-07 00:32:34 +0800

    2021.4.6

--- test/libarchive/main.cpp
+++ test/libarchive/main.cpp
@@ -1,3 +1,5 @@
+#include <fcntl.h>
+
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
@@ -39,7 +41,56 @@ std::int32_t copy_data(struct archive *ar, struct archive *aw) {
 }  // namespace
 
 // https://github.com/libarchive/libarchive/blob/master/examples/minitar/minitar.c
-void compress(const std::string &file_name) {}
+void compress(const std::string &file_name) {
+  auto archive = archive_write_new();
+  archive_write_set_format_gnutar(archive);
+  archive_write_add_filter_gzip(archive);
+
+  std::string out = file_name + ".tar.gz";
+  archive_write_open_filename(archive, out.c_str());
+
+  auto disk = archive_read_disk_new();
+  archive_read_disk_set_standard_lookup(disk);
+  auto r = archive_read_disk_open(disk, file_name.c_str());
+  if (r != ARCHIVE_OK) {
+    error(archive_error_string(disk));
+  }
+
+  while (true) {
+    auto entry = archive_entry_new();
+    r = archive_read_next_header2(disk, entry);
+    if (r == ARCHIVE_EOF) {
+      archive_entry_free(entry);
+      break;
+    }
+    if (r != ARCHIVE_OK) {
+      error(archive_error_string(disk));
+    }
+
+    archive_read_disk_descend(disk);
+
+    r = archive_write_header(archive, entry);
+    if (r != ARCHIVE_OK) {
+      error(archive_error_string(disk));
+    }
+
+    char buff[16384];
+    auto fd = open(archive_entry_sourcepath(entry), O_RDONLY);
+    auto len = read(fd, buff, sizeof(buff));
+    while (len > 0) {
+      archive_write_data(archive, buff, static_cast<std::size_t>(len));
+      len = read(fd, buff, sizeof(buff));
+    }
+
+    close(fd);
+    archive_entry_free(entry);
+  }
+  archive_read_close(disk);
+  archive_read_free(disk);
+
+  archive_write_close(archive);
+  archive_write_free(archive);
+}
 
 // https://github.com/libarchive/libarchive/wiki/Examples
 void decompress(const std::string &file_name) {
@@ -106,7 +157,8 @@ int main(int argc, char *argv[]) {
     error("the file not exists");
   }
 
-  if (file_name.ends_with("zip") || file_name.ends_with("tar.gz")) {
+  if (file_name.ends_with("zip") || file_name.ends_with("tar.gz") ||
+      file_name.ends_with("tgz")) {
     decompress(file_name);
   } else {
     compress(file_name);
