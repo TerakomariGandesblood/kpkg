0b746850a7109bf63aa48de9aee65971b67d085c
kaiser <1244713586@qq.com>
2021-04-11 15:03:47 +0800

    2021.4.11

--- cmake/Valgrind.cmake
+++ cmake/Valgrind.cmake
@@ -10,8 +10,8 @@ if(KPKG_VALGRIND)
   add_test(
     NAME ${TEST_EXECUTABLE}-valgrind
     COMMAND
-      ${VALGRIND_EXECUTABLE} --leak-check=full --leak-resolution=med
-      --track-origins=yes --vgdb=no --tool=memcheck --gen-suppressions=all
-      --error-exitcode=1 ./${TEST_EXECUTABLE}
+      ${VALGRIND_EXECUTABLE} --leak-check=full --show-leak-kinds=all
+      --leak-resolution=med --track-origins=yes --vgdb=no --tool=memcheck
+      --gen-suppressions=all --error-exitcode=1 ./${TEST_EXECUTABLE}
     WORKING_DIRECTORY ${KPKG_BINARY_DIR}/test/unit_test)
 endif()
--- test/unit_test/program.cpp
+++ test/unit_test/program.cpp
@@ -4,37 +4,9 @@
 
 #include "program.h"
 
-TEST(Program, Test1) {
+TEST(Program, Test) {
   std::int32_t argc = 5;
-  const char* argv[] = {"kpkg", "curl", "zstd", "zlib", "-install", nullptr};
-
-  kpkg::Program program(argc, const_cast<char**>(argv));
-
-  EXPECT_FALSE(program.use_proxy());
-  EXPECT_TRUE(program.install_package());
-  EXPECT_EQ(program.get_sanitize(), kpkg::Sanitize::None);
-
-  auto install = program.get_package_to_be_install();
-  EXPECT_EQ(std::size(install), 17);
-  EXPECT_EQ(install.front(), "sudo apt update");
-  EXPECT_EQ(install.back(),
-            "sudo update-alternatives --install /usr/bin/llvm-symbolizer "
-            "llvm-symbolizer /usr/bin/llvm-symbolizer-12 400");
-
-  auto dependency = program.get_dependency();
-  EXPECT_EQ(std::size(dependency), 2);
-  EXPECT_EQ(dependency.front().get_name(), "openssl");
-  EXPECT_EQ(dependency.back().get_name(), "zlib");
-
-  auto to_be_built = program.get_library_to_be_built();
-  EXPECT_EQ(std::size(to_be_built), 2);
-  EXPECT_EQ(to_be_built.front().get_name(), "curl");
-  EXPECT_EQ(to_be_built.back().get_name(), "zstd");
-}
-
-TEST(Program, Test2) {
-  std::int32_t argc = 4;
-  const char* argv[] = {"kpkg", "spdlog", "-proxy", "-memory", nullptr};
+  const char* argv[] = {"kpkg", "spdlog", "curl", "-proxy", "-memory", nullptr};
 
   kpkg::Program program(argc, const_cast<char**>(argv));
 
@@ -43,11 +15,14 @@ TEST(Program, Test2) {
   EXPECT_EQ(program.get_sanitize(), kpkg::Sanitize::Memory);
 
   auto dependency = program.get_dependency();
-  EXPECT_EQ(std::size(dependency), 2);
+  EXPECT_EQ(std::size(dependency), 4);
   EXPECT_EQ(dependency.front().get_name(), "libc++");
-  EXPECT_EQ(dependency.back().get_name(), "fmt");
+  EXPECT_EQ(dependency.at(1).get_name(), "fmt");
+  EXPECT_EQ(dependency.at(2).get_name(), "openssl");
+  EXPECT_EQ(dependency.back().get_name(), "zlib");
 
   auto to_be_built = program.get_library_to_be_built();
-  EXPECT_EQ(std::size(to_be_built), 1);
-  EXPECT_EQ(to_be_built.front().get_name(), "spdlog");
+  EXPECT_EQ(std::size(to_be_built), 2);
+  EXPECT_EQ(to_be_built.front().get_name(), "curl");
+  EXPECT_EQ(to_be_built.back().get_name(), "spdlog");
 }
