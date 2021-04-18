2422942f1f188a99b3121d532fad4e30612e09a7
kaiser <1244713586@qq.com>
2021-04-18 17:43:23 +0800

    2021.4.18

--- test/library/magic_enum/main.cpp
+++ test/library/magic_enum/main.cpp
@@ -7,13 +7,13 @@ enum class Color { Red = 2, Blue = 4, Green = 8 };
 
 int main() {
   {
-    auto color{Color::Red};
+    auto color = Color::Red;
     std::cout << magic_enum::enum_name(color) << '\n';
   }
 
   {
-    std::string color_name{"Green"};
-    auto color{magic_enum::enum_cast<Color>(color_name)};
+    std::string color_name = "Green";
+    auto color = magic_enum::enum_cast<Color>(color_name);
     if (color) {
       std::cout << "Ok\n";
     }
