221b785ec908ebf654748e853309804a908ecf97
kaiser <1244713586@qq.com>
2021-10-03 11:51:57 +0800

    2021.10.3

---
+++ test/extra_test/onetbb/main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cstdint>
+#include <execution>
+#include <random>
+#include <vector>
+
+#include "error.h"
+
+int main() {
+  std::vector<std::int32_t> v;
+  constexpr std::int32_t size = 100000;
+  v.reserve(size);
+
+  std::random_device rd;
+  std::default_random_engine gen(rd());
+  for (std::int32_t i = 0; i < size; ++i) {
+    v.push_back(static_cast<std::int32_t>(gen()));
+  }
+
+  std::sort(std::execution::par, std::begin(v), std::end(v));
+  EXPECT(std::is_sorted(std::execution::par, std::begin(v), std::end(v)));
+}
