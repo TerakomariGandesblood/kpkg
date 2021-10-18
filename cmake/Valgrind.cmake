899994763e631f26def51a6270bb51973bf6d7fa
kaiser <1244713586@qq.com>
2021-10-18 17:47:46 +0800

    2021.10.18

--- cmake/Valgrind.cmake
+++ cmake/Valgrind.cmake
@@ -14,4 +14,25 @@ if(KPKG_VALGRIND)
       --leak-resolution=med --track-origins=yes --vgdb=no --tool=memcheck
       --gen-suppressions=all --error-exitcode=1 ./${TEST_EXECUTABLE}
     WORKING_DIRECTORY ${KPKG_BINARY_DIR}/test/unit_test)
+
+  set(VALGRIND_CMD
+      ${VALGRIND_EXECUTABLE}
+      --leak-check=full
+      --show-leak-kinds=all
+      --leak-resolution=med
+      --track-origins=yes
+      --vgdb=no
+      --tool=memcheck
+      --gen-suppressions=all
+      --error-exitcode=1
+      ${KPKG_BINARY_DIR}/tool/${EXECUTABLE})
+
+  if(KPKG_TEST_USE_PROXY)
+    add_test(NAME kpkg-list-valgrind COMMAND ${VALGRIND_CMD} list -p)
+    add_test(NAME kpkg-install-valgrind COMMAND ${VALGRIND_CMD} install spdlog
+                                                -p)
+  else()
+    add_test(NAME kpkg-list-valgrind COMMAND ${VALGRIND_CMD} list)
+    add_test(NAME kpkg-install-valgrind COMMAND ${VALGRIND_CMD} install spdlog)
+  endif()
 endif()
--- include/version.h
+++ include/version.h
@@ -7,7 +7,7 @@
 
 #define KPKG_VER_MINOR 5
 
-#define KPKG_VER_PATCH 8
+#define KPKG_VER_PATCH 9
 
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
