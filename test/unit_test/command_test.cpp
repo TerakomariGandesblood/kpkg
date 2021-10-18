887ec2fe664c858fbb3cdc3947c6bf8cec47a491
kaiser <1244713586@qq.com>
2021-10-18 11:49:40 +0800

    2021.10.18

--- test/unit_test/command_test.cpp
+++ test/unit_test/command_test.cpp
@@ -5,5 +5,5 @@
 TEST_CASE("calc_command", "[command]") {
   REQUIRE(
       kpkg::detail::calc_command({"configure", "build", "install"}, "cwd") ==
-      R"(cd cwd && export CC=gcc-11 && export CXX=g++-11 && export CFLAGS="-fPIC" && export CXXFLAGS="-fPIC" && configure && build && install)");
+      R"(cd cwd && export CC=gcc-11 && export CXX=g++-11 && export CFLAGS="-fPIC -static-libgcc" && export CXXFLAGS="-fPIC -static-libgcc -static-libstdc++" && configure && build && install)");
 }
