accd3b82fa6dd6872a2091f3ef45ac7df4600ea5
kaiser <1244713586@qq.com>
2021-10-28 20:30:59 +0800

    2021.10.28

--- src/library.cpp
+++ src/library.cpp
@@ -102,7 +102,7 @@ void Library::download(const std::string& proxy) const {
       spdlog::info("Use proxy: {}", proxy);
     }
 
-    static HTTPDownloader downloader(proxy);
+    HTTPDownloader downloader(proxy);
     downloader.download(download_url_, file_name_);
 
     spdlog::info("Download file: {} complete", file_name_);
