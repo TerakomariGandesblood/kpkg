9073527fb208d2da5ea85811cd1265668d279c2c
kaiser <1244713586@qq.com>
2021-11-06 18:14:23 +0800

    2021.11.6

---
+++ test/extra_test/dbg_macro/main.cpp
@@ -0,0 +1,27 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include <dbg.h>
+
+std::int32_t factorial(std::int32_t n) {
+  if (dbg(n <= 1)) {
+    return dbg(1);
+  } else {
+    return dbg(n * factorial(n - 1));
+  }
+}
+
+int main() {
+  std::string message = "hello";
+  dbg(message);
+
+  const std::int32_t a = 2;
+  const std::int32_t b = dbg(3 * a) + 1;
+
+  std::vector<std::int32_t> numbers = {b, 13, 42};
+  dbg(numbers);
+
+  dbg("this line is executed");
+  factorial(4);
+}
