d8bf6f049a942855b85ac3f57219d73834ebb6b1
kaiser <1244713586@qq.com>
2021-12-18 15:59:43 +0800

    2021.12.18

--- script/build.sh
+++ script/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  curl -L https://github.com/KaiserLancelot/klib/releases/download/v0.10.4/klib-0.10.4-Linux.deb \
+  curl -L https://github.com/KaiserLancelot/klib/releases/download/v0.10.7/klib-0.10.7-Linux.deb \
     -o klib.deb
   sudo dpkg -i klib.deb
 else
