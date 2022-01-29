6623bed099972ae1f9b3b74e4b940062a981236a
kaiser <1244713586@qq.com>
2022-01-29 21:01:29 +0800

    2022.1.29

--- test/unit_test/command_test.cpp
+++ test/unit_test/command_test.cpp
@@ -5,5 +5,5 @@
 TEST_CASE("calc_command", "[command]") {
   REQUIRE(
       kpkg::detail::calc_command({"configure", "build", "install"}, "cwd") ==
-      R"(cd cwd && export CI=false && export CC=gcc-11 && export CXX=g++-11 && export CFLAGS="-fuse-ld=gold -march=x86-64 -mtune=generic -g0 -DNDEBUG -O3 -fPIC -static-libgcc" && export CXXFLAGS="-fuse-ld=gold -march=x86-64 -mtune=generic -g0 -DNDEBUG -O3 -fPIC -static-libgcc -static-libstdc++" && configure && build && install)");
+      R"(cd cwd && export CI=false && export CC=gcc-11 && export CXX=g++-11 && export CFLAGS="-march=x86-64 -mtune=generic -g0 -DNDEBUG -O3 -fPIC -static-libgcc" && export CXXFLAGS="-march=x86-64 -mtune=generic -g0 -DNDEBUG -O3 -fPIC -static-libgcc -static-libstdc++" && configure && build && install)");
 }
