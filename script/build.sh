79f81195bab04a47a2f8be78507bac10dfc1f641
kaiser <1244713586@qq.com>
2022-02-01 00:29:10 +0800

    2022.1.31

--- script/build.sh
+++ script/build.sh
@@ -6,6 +6,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   curl -L https://github.com/KaiserLancelot/klib/releases/download/v0.12.0/klib-0.12.0-Linux.deb \
     -o klib.deb
   sudo dpkg -i klib.deb
+
+  # FIXME
+  curl -L https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage \
+    -o appimagetool-x86_64.AppImage
+  sudo mkdir -p /root/.local/share/Nuitka/appimagetool-x86_64.AppImage/x86_64/13
+  sudo mv appimagetool-x86_64.AppImage /root/.local/share/Nuitka/appimagetool-x86_64.AppImage/x86_64/13/appimagetool-x86_64.AppImage
 else
   echo "The system does not support: $OSTYPE"
   exit 1
