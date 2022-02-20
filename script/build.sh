764e9aa6992755aa475c90e8f1a5736022d55899
kaiser <1244713586@qq.com>
2022-02-20 21:13:40 +0800

    2022.2.20

--- script/build.sh
+++ script/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  curl -L https://github.com/KaiserLancelot/klib/releases/download/v0.14.8/klib-0.14.8-Linux.deb \
+  curl -L https://github.com/KaiserLancelot/klib/releases/download/v0.14.9/klib-0.14.9-Linux.deb \
     -o klib.deb
   sudo dpkg -i klib.deb
 else
