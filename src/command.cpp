8f9d0d76bcf5e449fe3c0b0e942982bfab1aee55
Kaiser <1244713586@qq.com>
2022-03-15 21:25:40 +0800

    use '-fvisibility=hidden' build library

--- src/command.cpp
+++ src/command.cpp
@@ -25,10 +25,10 @@ std::string calc_command(const std::vector<std::string>& commands,
   cmd = combine_command(cmd, "export CC=gcc-11 && export CXX=g++-11");
   cmd = combine_command(
       cmd,
-      R"(export CFLAGS="-pipe -march=haswell -mtune=haswell -fno-plt -fno-math-errno -fno-trapping-math -fno-semantic-interposition -fipa-pta -fgraphite-identity -O3 -g0 -DNDEBUG -fPIC")");
+      R"(export CFLAGS="-pipe -march=haswell -mtune=haswell -fvisibility=hidden -fno-plt -fno-math-errno -fno-trapping-math -fno-semantic-interposition -fipa-pta -fgraphite-identity -O3 -g0 -DNDEBUG -fPIC")");
   cmd = combine_command(
       cmd,
-      R"(export CXXFLAGS="-pipe -march=haswell -mtune=haswell -fno-plt -fno-math-errno -fno-trapping-math -fno-semantic-interposition -fipa-pta -fgraphite-identity -O3 -g0 -DNDEBUG -fPIC")");
+      R"(export CXXFLAGS="-pipe -march=haswell -mtune=haswell -fvisibility=hidden -fno-plt -fno-math-errno -fno-trapping-math -fno-semantic-interposition -fipa-pta -fgraphite-identity -O3 -g0 -DNDEBUG -fPIC")");
   cmd = combine_command(
       cmd,
       R"(export LDFLAGS="-fuse-ld=bfd -static-libgcc -static-libstdc++ -s -L/usr/local/lib")");
