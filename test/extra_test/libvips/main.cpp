c89ea9706be2547dfd47bade41a9e3fb28999b8a
Kaiser <1244713586@qq.com>
2022-03-23 13:52:43 +0800

    fix

--- test/extra_test/libvips/main.cpp
+++ test/extra_test/libvips/main.cpp
@@ -1,24 +1,12 @@
 #include <klib/log.h>
 #include <vips/vips8>
 
-#ifdef KPKG_SANITIZER
-#include <sanitizer/lsan_interface.h>
-#endif
-
 int main() {
-#ifdef KPKG_SANITIZER
-  __lsan_disable();
-#endif
-
-  if (vips_init("")) {
+  if (VIPS_INIT("klib")) {
     klib::error("VIPS_INIT() failed");
   }
 
   auto image = vips::VImage::new_from_file("avatar.jpg");
   image.webpsave("avatar.wepb");
   image.jpegsave("avatar.jpeg");
-
-#ifdef KPKG_SANITIZER
-  __lsan_enable();
-#endif
 }
