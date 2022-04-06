d233573314313c1b7ba2a83574ce1515781e66be
Kaiser <kaiserlancelot123@gmail.com>
2022-04-06 12:03:57 +0800

    Update klib

--- script/build.sh
+++ script/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  curl -fsSL https://github.com/KaiserLancelot/klib/releases/download/v1.14.2/klib-1.14.2-Linux.deb \
+  curl -fsSL https://github.com/KaiserLancelot/klib/releases/download/v1.16.0/klib-1.16.0-Linux.deb \
     -o klib.deb
   sudo dpkg -i klib.deb
 else
